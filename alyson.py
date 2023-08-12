c35725c10e57561b8227fc07dd69e23b9e5be61b
alyson coppola <nylover85@gmail.com>
2023-08-12 04:49:06 +0000

    made changes

--- alyson.py
+++ alyson.py
@@ -1,4 +1,19 @@
-print('hello')
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+# Create 3 instances
+instances = ec2.create_instances(
+        ImageId="ami-08a52ddb321b32a8c",
+        MinCount=3,
+        MaxCount=3,
+        InstanceType="t2.micro",
+        SubnetId='subnet-064a5001084e7d154'
+    )
+
+# Print instance IDs
+for instance in instances:
+    print(instance.id)
 
 
 
